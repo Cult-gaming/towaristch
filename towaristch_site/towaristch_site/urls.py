cb0a17cf227ddb331404370321f63a873bfd2080
Maxim Sinyugin <m.sinyugin@rtk-dc.ru>
2019-02-13 17:36:17 +0300

    new initial

--- towaristch_site/towaristch_site/urls.py
+++ towaristch_site/towaristch_site/urls.py
@@ -1,30 +1,45 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
+"""towaristch_site URL Configuration
 
-from cms.sitemaps import CMSSitemap
-from django.conf import settings
-from django.conf.urls import include, url
-from django.conf.urls.i18n import i18n_patterns
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
+from django.urls import path
+from django.conf.urls import include
+from django.conf.urls import url
 
-admin.autodiscover()
+url(r'^weblog/', include('zinnia.urls')),
+url(r'^comments/', include('django_comments.urls')),
 
 urlpatterns = [
-    url(r'^sitemap\.xml$', sitemap,
-        {'sitemaps': {'cmspages': CMSSitemap}}),
+    path('admin/', admin.site.urls),
 ]
 
-urlpatterns += i18n_patterns(
-    url(r'^admin/', include(admin.site.urls)),  # NOQA
-    url(r'^', include('cms.urls')),
-)
+blog_urls = ([
+    url(r'^', include('zinnia.urls.capabilities')),
+    url(r'^search/', include('zinnia.urls.search')),
+    url(r'^sitemap/', include('zinnia.urls.sitemap')),
+    url(r'^trackback/', include('zinnia.urls.trackback')),
+    url(r'^blog/tags/', include('zinnia.urls.tags')),
+    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+    url(r'^blog/random/', include('zinnia.urls.random')),
+    url(r'^blog/authors/', include('zinnia.urls.authors')),
+    url(r'^blog/categories/', include('zinnia.urls.categories')),
+    url(r'^blog/comments/', include('zinnia.urls.comments')),
+    url(r'^blog/', include('zinnia.urls.entries')),
+    url(r'^blog/', include('zinnia.urls.archives')),
+    url(r'^blog/', include('zinnia.urls.shortlink')),
+    url(r'^blog/', include('zinnia.urls.quick_entry'))
+], 'zinnia')
 
-# This is only needed when using runserver.
-if settings.DEBUG:
-    urlpatterns = [
-        url(r'^media/(?P<path>.*)$', serve,
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        ] + staticfiles_urlpatterns() + urlpatterns
+url(r'^', include(blog_urls))
